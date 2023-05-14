a96a0ba79727b9b561116103bebeb8f1b353551a
Ella <ellacalugay@gmail.com>
2023-05-14 23:42:45 +0800

    Pseudocode

--- Test_television.py
+++ Test_television.py
@@ -1,1 +1,14 @@
 # Ella Lureen C. Calugay | Assignment 6 | UML Class Diagram
+
+# Pseudo code
+# Create TV 1
+# Turn on TV 1
+# Set the channel of TV 1 to 30
+# Set the volume of tv 1 to 3
+# Print the TV 1 status
+
+# Create TV 2
+# Turn on TV 2
+# Set the channel of TV 2 to 3
+# Set the volume of TV 2 to 2
+# Print the TV 2 status
