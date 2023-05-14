11686091d7aace69f0ce7f47e9fecbd8855d9bc8
Ella <ellacalugay@gmail.com>
2023-05-14 22:08:31 +0800

    Creating a parametrized constructor

--- Television.py
+++ Television.py
@@ -1,8 +1,12 @@
 # Ella Lureen C. Calugay | Assignment 6 | UML Class Diagram
 
 # Pseudocode
-# Create a Class TV 
+# Create a class named TV
 class TV:
-# Create a parametrized constructor
+    # Create a parametrized constructor
+    def __init__(self, channel, volume_level, switch_button):
+        self.channel=1
+        self.volume_level=1
+        self.switch_button=False
 # List all the methods that are needed
 # End of code.
