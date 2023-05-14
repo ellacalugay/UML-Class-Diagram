be6ad31f4574e4e8a980ef853a3b28b3d58b8490
Ella <ellacalugay@gmail.com>
2023-05-14 21:55:17 +0800

    Pseudocode

--- Television.py
+++ Television.py
@@ -1,1 +1,7 @@
 # Ella Lureen C. Calugay | Assignment 6 | UML Class Diagram
+
+# Pseudocode
+# Create a Class TV 
+# Create a parametrized constructor
+# List all the methods that are needed
+# End of code.
