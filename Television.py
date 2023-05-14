0c744162e19f775eddd6dbe6c959901316bced5f
Ella <ellacalugay@gmail.com>
2023-05-14 22:38:27 +0800

    Create a get channel method

--- Television.py
+++ Television.py
@@ -8,11 +8,18 @@ class TV:
         self.channel = 1
         self.volume_level = 1
         self.switch_button = False
+
 # List all the methods that are needed
     # Create a turn on method
     def turn_on(self):
         self.switch_button = True
+
     # Create a turn off method
     def turn_off(self):
         self.switch_button = False
+
+    # Create a get channel method
+    def get_channel(self):
+        return self.channel
+
 # End of code.
